09315d9212976d0592b0bab9800c72988c991627
Aden <abdi.aden@verizon.com>
2022-12-30 13:51:19 -0600

    created a user class

--- Backend/main.py
+++ Backend/main.py
@@ -5,4 +5,28 @@ import sqlalchemy.ext.declarative as _declarative
 
 Base = _declarative.declarative_base()
 
+engine = _sql.create_engine('sqlite:///database.db', echo=True)
+Base.metadata.create_all(engine)    # Create tables
+Session = _orm.sessionmaker(bind=engine)
+session = Session()
 
+# Path: Backend\main.py
+
+from sqlalchemy import Column, Integer, String
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    fullname = Column(String)
+    password = Column(String)
+
+    def __init__(self, name, fullname, password):
+        self.name = name
+        self.fullname = fullname
+        self.password = password
+
+    def __repr__(self):
+        return
+
+        
